dd003beb07bb49370dd9108d7fc97de6b6e32ced
Natalie Scoobs <c3142113@gmail.com>
2018-01-16 20:44:31 +1100

    UI changes to make it less ugly

--- TaikoSheetViewer/Form1.cs
+++ TaikoSheetViewer/Form1.cs
@@ -16,13 +16,22 @@ using OpenTK;
 namespace WindowsFormsApp1 {
     public partial class Form1 : Form {
         SKControl skC = new SKControl();
-        SongData s;
 
-        //Current X Position
+        SongData sheet;
+
+        SKRect BarRect;
+        SKPaint skP;
+
+        //Current X position
         long cXPos = 0;
 
+        //Last Mouse X position
+        long lastMouseX = 0;
+
         //the current scale
-        int scale = 80;
+        int scale = 200;
+
+        bool isMouseDown = false;
 
         public Form1() {
             InitializeComponent();
@@ -36,33 +45,32 @@ namespace WindowsFormsApp1 {
             skC.MouseLeave += Form1_MouseLeave;
         }
 
-        //Form has loaded, set 
+        //Form has loaded
         private void Form1_Load(object sender, EventArgs e) {
             //temporary hard coded meemee
             string fn = @"J:\Taiko\wii5 sheets\sheet\newsht\solo\linda_h.bin";
             
             //read the sheet
-            s = NewShtReader.ReadSheet(fn);
+            sheet = NewShtReader.ReadSheet(fn);
 
             //Get the total note count for ~statistics~
             int nc = 0;
-            foreach (Bar b in s.Bars) { nc += b.NoteCount; }
+            foreach (Bar b in sheet.Bars) { nc += b.NoteCount; }
             
             //Change the window title to show the file name, bar count and note count
-            this.Text = fn + " " + s.BarCount +" bars | " + nc + " notes";
-            
+            Text = fn + " " + sheet.BarCount +" bars | " + nc + " notes";
+
+            BarRect = new SKRect(0, 0, Size.Width, 30);
+
             //set up the Skia graphics control and its paint event
-            skC.Size = this.Size;
+            skC.Size = Size;
             skC.PaintSurface += Gl_PaintSurface;
+
             
             //Add the Skia control to the form
             Controls.Add(skC);
         }
         
-        bool isMouseDown = false;
-        long lastMouseX = 0;
-
-
         private void Form1_MouseMove(object sender, MouseEventArgs e) {
             //if the mouse is down, move the current position accordingly
             if (isMouseDown) {
@@ -84,12 +92,23 @@ namespace WindowsFormsApp1 {
         //Obviously, disable the mouse scrolling if the mouse leaves the form
         private void Form1_MouseLeave(object sender, EventArgs e) { isMouseDown = false; }
 
+
         //Draw the ugly circles that make up the viewer
         private void Gl_PaintSurface(object sender, SKPaintSurfaceEventArgs e) {
+            skP = new SKPaint();
             //Clear the canvas, of course
             e.Surface.Canvas.Clear(SKColor.Parse("AAAAAA"));
+            e.Surface.Canvas.DrawLine(
+                0,
+                30,
+                Size.Width,
+                30, new SKPaint());
 
-            //TODO::OPTIMIZE AND REMOVE ALL COPY PASTE SHITE
+            skP.Color = SKColor.Parse("333333");
+
+            e.Surface.Canvas.DrawRect(BarRect, skP);
+
+            //TODO::OPTIMIZE AND REMOVE ALL COPY PASTE SHITE IN HERE
             //TODO::ADD ZOOM
             //TODO::FIND THE REST OF THE NOTE TYPES
             //TODO::TRY SONGS LIKE THAT ONE WITH THE BEEPS THAT CHANGES PATH
@@ -97,84 +116,93 @@ namespace WindowsFormsApp1 {
             //TODO::FINISH COMMENTS
 
             //For each of the bars
-            for (int i = 0; i < s.BarCount; i++) {
+            for (int i = 0; i < sheet.BarCount; i++) {
+                skP.StrokeWidth = 1;
+
+                skP.Color = SKColor.Parse("AAAAAA");
                 e.Surface.Canvas.DrawLine(
-                    cXPos + ((s.Bars[i].Timecode / 1000) * scale), 
-                    0, cXPos + ((s.Bars[i].Timecode / 1000) * scale), 
-                    30, new SKPaint());
+                    cXPos + ((sheet.Bars[i].Timecode / 1000) * scale), 
+                    0, 
+                    cXPos + ((sheet.Bars[i].Timecode / 1000) * scale), 
+                    30, skP);
+
+                skP.Color = SKColor.Parse("222222");
+                e.Surface.Canvas.DrawLine(
+                    cXPos + ((sheet.Bars[i].Timecode / 1000) * scale),
+                    30,
+                    cXPos + ((sheet.Bars[i].Timecode / 1000) * scale),
+                    Size.Height, skP);
 
                 e.Surface.Canvas.DrawText(
                     "Bar " + i.ToString(), 
-                    cXPos + ((s.Bars[i].Timecode / 1000) * scale), 
-                    87, new SKPaint());
+                    cXPos + ((sheet.Bars[i].Timecode / 1000) * scale) + 3, 
+                    87, skP);
 
                 e.Surface.Canvas.DrawText(
-                    s.Bars[i].Speed.ToString("0.00"), 
-                    cXPos + ((s.Bars[i].Timecode / 1000) * scale), 
-                    100, new SKPaint());
+                    sheet.Bars[i].Speed.ToString("0.00"), 
+                    cXPos + ((sheet.Bars[i].Timecode / 1000) * scale) + 3, 
+                    100, skP);
 
-                foreach (Note n in s.Bars[i].Notes) {
+                foreach (Note n in sheet.Bars[i].Notes) {
                     string type = NewShtReader.GetNoteType(n.NoteType);
-
-                    SKPaint p = new SKPaint();
-
-                    p.StrokeWidth = (type.StartsWith("Large") ? 16 : 10);
+                    
+                    skP.StrokeWidth = (type.StartsWith("Large") ? 16 : 10);
 
                     if (type == "Don" || type == "LargeDon") {
-                        p.Color = SKColor.Parse("FF0000");
+                        skP.Color = SKColor.Parse("DD4444");
                     } else if (type == "Katsu" || type == "LargeKatsu") {
-                        p.Color = SKColor.Parse("0000FF");
+                        skP.Color = SKColor.Parse("4444DD");
                     } else if (type == "Roll" || type == "LargeRoll") {
-                        p.Color = SKColor.Parse("FFFF00");
+                        skP.Color = SKColor.Parse("DDDD44");
 
                         e.Surface.Canvas.DrawText(
                             NewShtReader.GetNoteType(n.NoteType),
-                            cXPos + ((s.Bars[i].Timecode + n.OffsetMs) / 1000) * scale,
-                            50, p);
+                            cXPos + ((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) * scale,
+                            50, skP);
 
                         e.Surface.Canvas.DrawText(
                             n.NoteLength.ToString() + "ms", 
-                            cXPos + ((s.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
-                            65, p);
+                            cXPos + ((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
+                            65, skP);
 
                         e.Surface.Canvas.DrawLine(
-                            cXPos + ((s.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
-                            14.5f, 
-                            cXPos + (((s.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale, 
-                            14.5f, p);
+                            cXPos + ((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
+                            16f, 
+                            cXPos + (((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale, 
+                            16f, skP);
 
                         e.Surface.Canvas.DrawCircle(
-                            cXPos + (((s.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale, 
-                            15, 
-                            (type.StartsWith("Large") ? 8 : 5), p);
+                            cXPos + (((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale, 
+                            16, 
+                            (type.StartsWith("Large") ? 8 : 5), skP);
                     } else { 
-                        p.Color = SKColor.Parse("FFCC00");
+                        skP.Color = SKColor.Parse("FFCC00");
 
                         e.Surface.Canvas.DrawText(
                             NewShtReader.GetNoteType(n.NoteType), 
-                            cXPos + ((s.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
-                            50, p);
+                            cXPos + ((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
+                            50, skP);
 
                         e.Surface.Canvas.DrawText(
                             n.RequiredHits.ToString() + " hits in " + n.NoteLength.ToString() + "ms", 
-                            cXPos + ((s.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
-                            65, p);
+                            cXPos + ((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
+                            65, skP);
 
                         e.Surface.Canvas.DrawLine(
-                            cXPos + ((s.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
-                            14.5f, 
-                            cXPos + (((s.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale, 14.5f, p);
+                            cXPos + ((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
+                            16, 
+                            cXPos + (((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale, 16, skP);
 
                         e.Surface.Canvas.DrawCircle(
-                            cXPos + (((s.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale,
-                            15,
-                            (type.StartsWith("Large") ? 8 : 5), p);
+                            cXPos + (((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) + (n.NoteLength / 1000)) * scale,
+                            16,
+                            (type.StartsWith("Large") ? 8 : 5), skP);
                     }
 
                     e.Surface.Canvas.DrawCircle(
-                        cXPos + ((s.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
-                        15, 
-                        (type.StartsWith("Large") ? 8 : 5), p);
+                        cXPos + ((sheet.Bars[i].Timecode + n.OffsetMs) / 1000) * scale, 
+                        16, 
+                        (type.StartsWith("Large") ? 8 : 5), skP);
                 }
             }
         }
